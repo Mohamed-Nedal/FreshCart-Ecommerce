c74f810faf3cbebea1d968b18dfc5da827657f71
mohamednedal435 <mo7mednedal@gmail.com>
2024-09-05 15:37:46 +0300

    Edit Logo Src

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import logoImg from "../../assets/freshcart-logo.svg";
 
 const Logo = () => {
   return (
     <Link to={"/"} className="flex items-center gap-1 font-bold capitalize">
-      <img src="/src/assets/freshcart-logo.svg" alt="logo" />
+      <img src={logoImg} alt="logo" />
     </Link>
   );
 };
